17206ecb19558411039e63b4ac92b55758682047
gcp91020 <92786423+gcp91020@users.noreply.github.com>
2021-10-23 18:51:35 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -38,5 +38,6 @@ sed -i 's=dnsmasq \\=#dnsmasq \\=' include/target.mk
 
 if [[ "$MOD"  == "mt7621" ]]; then
   sed -i "s/kmod-mt7603/kmod-mt7603e/" target/linux/ramips/image/mt7621.mk
+  grep mt7603 target/linux/ramips/image/mt7621.mk 
 fi
 
