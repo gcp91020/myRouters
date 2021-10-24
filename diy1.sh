74a7f91341f5bd5e13a2358bd7094f22d679ebb5
gcp91020 <92786423+gcp91020@users.noreply.github.com>
2021-10-24 09:19:51 +0800

    Create diy1.sh

---
+++ diy1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+
+CONF_FILE=".config"
+MOD=`egrep "^CONFIG_TARGET_ramips_[^_]+=y" $CONF_FILE`
+MOD=`awk -v FS='[_=\n]' '{print $4}' <<< $MOD`
+
+if [[ "$MOD"  == "mt7621" ]]; then
+  sed -i "s/kmod-mt7603/kmod-mt7603e/" target/linux/ramips/image/mt7621.mk
+  grep mt7603 target/linux/ramips/image/mt7621.mk 
+fi
