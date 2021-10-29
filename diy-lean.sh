6ea61459985283822f7dd315ad60e8b84590dd47
gcp91020 <92786423+gcp91020@users.noreply.github.com>
2021-10-29 15:06:19 +0800

    Update diy-lean.sh

--- diy-lean.sh
+++ diy-lean.sh
@@ -9,7 +9,9 @@ MOD=`awk -v FS='[_=\n]' '{print $4}' <<< $MOD`
 if [[ "$MOD"  == "mt7621" ]]; then
   #sed -i "s/kmod-mt7603/kmod-mt7603e/" target/linux/ramips/image/mt7621.mk
   #grep mt7603 target/linux/ramips/image/mt7621.mk 
+  git clone https://github.com/openwrt/openwrt openwrt-origin
   rm -rf toolchain/musl
-  /bin/cp -rf ../musl toolchain
+  /bin/cp -rf openwrt-origin/toolchain/musl toolchain
+  rm -rf openwrt-origin
   ls -l toolchain
 fi
