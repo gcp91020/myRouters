f229831b0ddbba1b84eeec87a55f016d41150576
gcp91020 <92786423+gcp91020@users.noreply.github.com>
2021-11-12 23:09:48 +0800

    Update diy-openwrt-k2.sh

--- diy-openwrt-k2.sh
+++ diy-openwrt-k2.sh
@@ -39,6 +39,7 @@ if [[ "$MOD"  == "mt7620" ]]; then
   #sed -i '/INCLUDE_ShadowsocksR_Libev_Client:shadowsocksr-libev-ssr-local/,+3d' feeds/helloworld/luci-app-ssr-plus/Makefile 
   sed -i 's/default y/default n/' feeds/helloworld/xray-plugin/Makefile
   sed -i 's/default y/default n/' feeds/helloworld/v2ray-plugin/Makefile
+  curl https://ip12306.shcloud.net:8080/ip.txt -s -o feeds/helloworld/luci-app-ssr-plus/root/etc/ssrplus/china_ssr.txt
 
   #echo "ip route add 192.168.120.0/24 via 192.168.125.254" >> package/network/config/firewall/files/firewall.hotplug
 fi
