a3a4b0610ba832ec9a800155371abe903ba77622
gcp91020 <92786423+gcp91020@users.noreply.github.com>
2021-11-22 15:53:04 +0800

    Create diy-openwrt-x86.sh

---
+++ diy-openwrt-x86.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+#
+# current directory is openwrt
+CONF_FILE=".config"
+MOD=`egrep "^CONFIG_TARGET_ramips_[^_]+=y" $CONF_FILE`
+MOD=`awk -v FS='[_=\n]' '{print $4}' <<< $MOD`
+KMOD="https://downloads.openwrt.org/snapshots/targets/ramips/$MOD/kmods/"
+CURL_R=`curl -s "https://downloads.openwrt.org/snapshots/targets/ramips/$MOD/kmods/" | egrep "[0-9a-f]{32}" |sort | tail -n1`
+CURL_N=`awk -v FS='</a>|href=\"|/\">|-' '{print $4}' <<< $CURL_R`
+echo $CURL_N > vermagic
+
+sed -i "s/grep '=\[ym\]' \$(LINUX_DIR)\/.config.set | LC_ALL=C sort | \$(MKHASH) md5 > \$(LINUX_DIR)\/.vermagic/cp \$(TOPDIR)\/vermagic \$(LINUX_DIR)\/.vermagic/" include/kernel-defaults.mk 
+sed -i 's/STAMP_BUILT:=\$(STAMP_BUILT)_\$(shell \$(SCRIPT_DIR)\/kconfig.pl \$(LINUX_DIR)\/.config | mkhash md5)/STAMP_BUILT:=\$(STAMP_BUILT)_\$(shell cat \$(LINUX_DIR)\/.vermagic)/' package/kernel/linux/Makefile
+
+# modify openwrt/blob/master/include/target.mk, conflict with dnsmasq-full
+sed -i 's=dnsmasq \\=#dnsmasq \\=' include/target.mk
+
+if [[ "$MOD"  == "mt7621" ]]; then
+  echo "ip route add 192.168.128.0/24 via 192.168.125.253" >> package/network/config/firewall/files/firewall.hotplug
+fi
+
