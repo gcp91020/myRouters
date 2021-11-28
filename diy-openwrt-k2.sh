914b13abb52e19239d014078793fbd4cd5888f97
gcp91020 <92786423+gcp91020@users.noreply.github.com>
2021-11-28 19:46:34 +0800

    Update diy-openwrt-k2.sh

--- diy-openwrt-k2.sh
+++ diy-openwrt-k2.sh
@@ -54,7 +54,7 @@ if [[ "$MOD"  == "mt7620" ]]; then
   sed -i '/prompt "Enable IPv6 support in packages"/{n;d}' ./config/Config-build.in
   sed -i '/prompt "Enable IPv6 support in packages"/a \\t\tdefault n' ./config/Config-build.in
   sed -i '/DEVICE_VARIANT:= v22.4 or older/{n;d}' target/linux/ramips/image/mt7620.mk
-  sed -i '/DEVICE_VARIANT:= v22.4 or older/a \\tDEVICE_PACKAGES := kmod-mt76x2 -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only -ppp -ppp-mod-pppoe' target/linux/ramips/image/mt7620.mk 
+  sed -i '/DEVICE_VARIANT:= v22.4 or older/a \ \ DEVICE_PACKAGES := kmod-mt76x2 -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only -ppp -ppp-mod-pppoe' target/linux/ramips/image/mt7620.mk 
   
   #安装ssr plus，但是不安装 ssr
   sed -i 's=+shadowsocksr-libev-ssr-check ==' feeds/helloworld/luci-app-ssr-plus/Makefile
