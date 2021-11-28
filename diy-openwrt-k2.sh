3827b6cadbce759c25939bca540cbd4eefb46208
gcp91020 <92786423+gcp91020@users.noreply.github.com>
2021-11-28 17:23:03 +0800

    Update diy-openwrt-k2.sh

--- diy-openwrt-k2.sh
+++ diy-openwrt-k2.sh
@@ -16,7 +16,7 @@ if [[ "$VER" == "" ]]; then
 else
   ver=`echo $VER | sed -e "s/.*CONFIG_LINUX_//" | sed "s/=y//" | sed "s/_/./"`
 fi
-CURL_R=`curl -s "https://downloads.openwrt.org/snapshots/targets/x86/64/kmods/" | egrep $ver | egrep "[0-9a-f]{32}" `
+CURL_R=`curl -s "https://downloads.openwrt.org/snapshots/targets/ramips/mt7620/kmods/" | egrep \>$ver | egrep "[0-9a-f]{32}" `
 DATE_1=`echo $CURL_R | sed  "s/<\/tr>/<\/tr>\n/g" | grep $ver | egrep "[0-9a-f]{32}" | awk -v FS='Mon |Tue |Wed |Thu |Fri |Sat |Sun |</td></tr>' '{print $2}' | sed -E "s/ ([0-9]) / 0\1 /" | awk -F ' ' '{print $4",-"$1"-,"$2","$3}' | sed -e "s/Jan/01/" | sed -e "s/Feb/02/" | sed -e "s/Mar/03/" | sed -e "s/Apr/04/" | sed -e "s/May/05/" | sed -e "s/Jun/06/" | sed -e "s/Jul/07/" | sed -e "s/Aug/08/" | sed -e "s/Sep/09/" | sed -e "s/Oct/10/" | sed -e "s/Nov/11/" | sed -e "s/Dec/12/"  | sort | tail -n1 | sed -e "s/-01-/Jan/" | sed -e "s/-02-/Feb/" | sed -e "s/-03-/Mar/" | sed -e "s/-04-/Apr/" | sed -e "s/-05-/May/" | sed -e "s/-06-/Jun/" | sed -e "s/-07-/Jul/" | sed -e "s/-08-/Aug/" | sed -e "s/-09-/Sep/" | sed -e "s/-10-/Oct/" | sed -e "s/-11-/Nov/" | sed -e "s/-12-/Dec/" | awk -F ',' '{print $2" "$3" "$4" "$1}' | sed -E "s/ 0([0-9]) / \1 /" `
 echo $DATE_1
 CURL_R1=`echo $CURL_R | sed  "s/<\/tr>/\n/g" | grep "$DATE_1" `
