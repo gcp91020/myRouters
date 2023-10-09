78e69495e419923e3f994178d3b34cecdf72c745
gcp91020 <92786423+gcp91020@users.noreply.github.com>
2023-10-09 13:28:07 +0800

    Update diy-openwrt-d2.sh

--- diy-openwrt-d2.sh
+++ diy-openwrt-d2.sh
@@ -29,3 +29,4 @@ if [[ -e files ]]; then
   curl -s https://raw.githubusercontent.com/gcp91020/myRouters/main/common_files/china_ip.txt -o files/etc/ssrplus/china_ssr.txt
   curl -s https://raw.githubusercontent.com/gcp91020/myRouters/main/common_files/dnsmasq.conf  -o files/etc/ssrplus/gfw_list.conf
 fi
+sed -i 's/PKG_USE_MIPS16/PKG_BUILD_FLAGS:=no-mips16\nPKG_USE_MIPS16/' feeds/helloworld/v2ray-plugin/Makefile 
